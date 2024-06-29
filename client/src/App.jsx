d6f200c76b7077b23977562407aabed79107880a
Senathirajah-Piranavan <piranavansenathirajah005@gmail.com>
2024-06-29 11:35:45 +0530

    commit 3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
   const [users, setUsers] = useState([]);
   const [filterUsers, setFilterUsers] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userData, setUserData] = useState({ name: "", age: "", district: "" });
 
   const getAllUsers = async () => {
     try {
@@ -20,57 +22,126 @@ function App() {
     getAllUsers();
   }, []);
 
-  // Search function
   const handleSearchChange = (e) => {
     const searchText = e.target.value.toLowerCase();
     const filteredUsers = users.filter((user) =>
       user.name.toLowerCase().includes(searchText) || 
-      user.district.toLowerCase().includes(searchText)
-    );
+      user.district.toLowerCase().includes(searchText));
     setFilterUsers(filteredUsers);
+  };
+
+  const handleDelete = async (id) => {
+    const isConfirmed = window.confirm('Are you sure you want to delete this user?');
+    if (isConfirmed) {
+      try {
+        await axios.delete(`http://localhost:8000/users/${id}`);
+        getAllUsers();
+      } catch (error) {
+        console.error("Error deleting user:", error);
+      }
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    getAllUsers();
+  };
+
+  const handleAddRecord = () => {
+    setUserData({ name: "", age: "", district: "" });
+    setIsModalOpen(true);
+  };
+
+  const handleData = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (userData.id) {
+        await axios.patch(`http://localhost:8000/users/${userData.id}`, userData);
+      } else {
+        await axios.post("http://localhost:8000/users", userData);
+      }
+      closeModal();
+      setUserData({ name: "", age: "", district: "" });
+    } catch (error) {
+      console.error("Error adding/updating user:", error);
+    }
+  };
+
+  const handleUpdateRecord = (user) => {
+    setUserData(user);
+    setIsModalOpen(true);
   };
 
   return (
     <>
-      <div className='container'>
-        <h2>CRUD application with ReactJs frontend and NodeJs backend</h2>
-      </div>
-      <div className="input-search">
-        <input 
-          type='search' 
-          placeholder="Search Text Here" 
-          onChange={handleSearchChange}
-        />
-        <button className='btn green'>Add Record</button>
-      </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Age</th>
-            <th>District</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filterUsers && filterUsers.map((user, index) => (
-            <tr key={user.id}>
-              <td>{index + 1}</td>
-              <td>{user.name}</td>
-              <td>{user.age}</td>
-              <td>{user.district}</td>
-              <td>
-                <button className='btn green'>Edit</button>
-              </td>
-              <td>
-                <button className='btn red'>Delete</button>
-              </td>
+      <div>
+        <div className='container'>
+          <h2>CRUD application with ReactJs frontend and NodeJs backend</h2>
+        </div>
+        <div className="input-search">
+          <input 
+            type='search' 
+            placeholder="Search Text Here" 
+            onChange={handleSearchChange}
+          />
+          <button className='btn green' onClick={handleAddRecord}>Add Record</button>
+        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Age</th>
+              <th>District</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filterUsers && filterUsers.map((user, index) => (
+              <tr key={user.id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+                <td>{user.district}</td>
+                <td>
+                  <button className='btn green' onClick={() => handleUpdateRecord(user)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(user.id)} className='btn red'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {isModalOpen && (
+          <div className="model">
+            <div className="model-content">
+              <span className="close" onClick={closeModal}>&times;</span>
+              <h2>{userData.id ? "Update Record" : "Add Record"}</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                  <label htmlFor="name">Full Name</label>
+                  <input type="text" value={userData.name} name="name" id='name' onChange={handleData} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="age">Age</label>
+                  <input type="number" value={userData.age} name="age" id='age' onChange={handleData} />
+                </div>
+                <div className="input-group">
+                  <label htmlFor="district">District</label>
+                  <input type="text" value={userData.district} name="district" id='district' onChange={handleData} />
+                </div>
+                <button className="btn green" type="submit">{userData.id ? "Update Record" : "Add Record"}</button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>  
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,17 +1,75 @@
 const express = require('express');
-const users= require('./sample.json');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+app.use(express.json());
 const port = 8000;
+
 app.use(cors({
-     origin : "http://localhost:5173",
-     methods : ['GET', 'POST', 'PATCH', 'DELETE'],
+  origin: "http://localhost:5173",
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
 }));
 
+const usersFilePath = path.join(__dirname, 'sample.json');
+
+const getUsersFromFile = () => {
+  const data = fs.readFileSync(usersFilePath);
+  return JSON.parse(data);
+};
+
+const saveUsersToFile = (users) => {
+  fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+};
+
 // Display all users
-app.get("/users",(req, res) =>{
-     return res.json(users);
+app.get("/users", (req, res) => {
+  const users = getUsersFromFile();
+  res.json(users);
 });
 
-app.listen(port, (err) => {console.log(`App  is running in port ${port}`);});
+// Delete user details
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  let users = getUsersFromFile();
+  users = users.filter((user) => user.id !== id);
+  saveUsersToFile(users);
+  res.json(users);
+});
+
+// Add New User
+app.post("/users", (req, res) => {
+  const { name, age, district } = req.body;
+  if (!name || !age || !district) {
+    return res.status(400).send({ message: "All Fields Required" });
+  }
+  const users = getUsersFromFile();
+  const id = Date.now();
+  users.push({ id, name, age, district });
+  saveUsersToFile(users);
+  res.json({ message: "User Details added successfully" });
+});
+
+// Update Users
+app.patch("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const { name, age, district } = req.body;
+  if (!name || !age || !district) {
+    return res.status(400).send({ message: "All Fields Required" });
+  }
+
+  let users = getUsersFromFile();
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    users[index] = { id, name, age, district };
+    saveUsersToFile(users);
+    res.json({ message: "User Details updated successfully" });
+  } else {
+    res.status(404).send({ message: "User not found" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
